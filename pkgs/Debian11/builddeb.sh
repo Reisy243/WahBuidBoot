d22d6f61b30366ae383750f3af4516707dc39b2d
Reisy243 <reisimineredpanda@gmail.com>
2023-05-13 16:12:58 -0600

    WahBuildBoot Alpha Deb package

--- pkgs/Debian11/builddeb.sh
+++ pkgs/Debian11/builddeb.sh
@@ -1,8 +1,12 @@
-srcurl="https://raw.githubusercontent.com/Reisy243/WahBuidBoot/main/"
-curl -O $srcurl/Panwah.tar.gz $srcurl/wbb $srcurl/pkgs/Debian11/control
+srcurl="https://raw.githubusercontent.com/Reisy243/WahBuidBoot/main"
+#curl -O $srcurl/Panwah.tar.gz -O $srcurl/wbb -O $srcurl/pkgs/Debian11/control
+if [ -d pkg ]; then
+	rm -rvf pkg
+fi
 mkdir -p pkg/DEBIAN pkg/usr/bin pkg/usr/share/wbb
 mv control pkg/DEBIAN
-mv wbb pkg/usb/bin
+mv wbb pkg/usr/bin
 tar -xvf Panwah.tar.gz
 mv Panwah pkg/usr/share/wbb
+
 dpkg-deb --build pkg
